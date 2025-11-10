b412d3402378d8532406d722015ed2d39516b33c
ajaymahto5521 <ajay.mahto@cctech.co.in>
2025-11-10 18:29:06 +0530

    Add Array class with operator[] for indexing
    Implement array class with operator overloading for indexing.

---
+++ Operator_Overloading_5exmples/Example4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Array {
+    int arr[5];
+public:
+    Array() { for(int i=0; i<5; ++i) arr[i] = i*10; }
+
+    int operator[](int index) const {
+        if(index < 0 || index >= 5) {
+            cout << "Index out of range!\n";
+            return -1;
+        }
+        return arr[index];
+    }
+};
+
+int main() {
+    Array a;
+    cout << a[2] << endl;   // Valid
+    cout << a[6] << endl;   // Invalid
+}
